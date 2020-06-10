dac0c67a98d93b2d589882957c572aad6807cda9
Vishal Sanghi <vishalsanghi@gmail.com>
2020-06-10 13:13:31 +0000

    Updated Script and additional folders

--- wp-bulk-localfolder-plugin-install.sh
+++ wp-bulk-localfolder-plugin-install.sh
@@ -10,40 +10,55 @@ WPPATH=<your-wordpress-installation-path>
 # WPI CLI Installation File Name
 FILE=wp-cli.phar
 
-# Local Plugin Path
-pluginpath=<path-to-store-plugins>
+# Local Plugin Path. Keep your plugins in this folder
+pluginpath=./plugins
 
-# Installed Local Plugin Path
-ipluginpath=<path-to-move-installed-plugins>
+# Installed Local Plugin Path. Your installed plugin will be in this folder
+ipluginpath=./installedplugins
 
 localpluginupdate() {
-		echo
-		echo "========================================================"
+	echo
+	echo "========================================================"
         echo "Plugin Update Operation from Local Folder Path Started"
         echo "========================================================"
         echo
-		for file in $pluginpath/*; do
-		echo "Initiating the plugin installation from local folder path"
-		plugin=$pluginpath/$(basename "$file")
-		wp plugin install --force $plugin --activate --path=$WPPATH --allow-root
-		echo "Plugin $(basename "$file") related operation completed"
-		if [ -d "$ipluginpath" ]
-				then
-						cd $pluginpath
-						mv $pluginpath/$(basename "$file") $ipluginpath/
-						echo "Moved plugin zip file to $ipluginpath"
-				else
-						mkdir $ipluginpath
-						mv $pluginpath/$(basename "$file") $ipluginpath/
-						echo "Moved plugin zip file to $ipluginpath"
-		fi
-		echo
-		echo "========================================================"
+	for file in $pluginpath/*; do
+	echo "Initiating the plugin installation from local folder path"
+	plugin=$pluginpath/$(basename "$file")
+	wp plugin install --force $plugin --activate --path=$WPPATH --allow-root
+	echo "Plugin $(basename "$file") related operation completed"
+	if [ -d "$ipluginpath" ]
+		then
+			cd $pluginpath
+			mv $pluginpath/$(basename "$file") $ipluginpath/
+			echo "Moved plugin zip file to $ipluginpath"
+		else
+			mkdir $ipluginpath
+			mv $pluginpath/$(basename "$file") $ipluginpath/
+			echo "Moved plugin zip file to $ipluginpath"
+	fi
+	echo
+	echo "========================================================"
         echo "Plugin Update Operation from Local Folder Path Completed"
         echo "========================================================"
         echo
 	done
 }
+
+wpcliinstall() {
+ 	# Starting the Wordpress installation process using wp-cli
+	echo "===================================="
+	echo "Please wait while we install wp-cli"
+	echo "===================================="
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	chmod +x wp-cli.phar
+	cp wp-cli.phar /usr/bin/wp
+	echo "=========================="
+	echo "Finished installing wp-cli"
+	echo "=========================="
+	echo
+}
+
 
 if [ -f "$FILE" ]
     then
